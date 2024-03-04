8c1edcafa1c8373c542371cf4088bc63636a0c67
CyberBread <ya.bolotov-ilya@yandex.ru>
2024-03-04 21:52:49 +0300

    add shoot vfx

--- Assets/Scripts/AnimationEventsHandler.cs
+++ Assets/Scripts/AnimationEventsHandler.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class AnimationEventsHandler : MonoBehaviour
 {
+    public event Action OnReloadAnimationEnd;
     public event Action OnShootAnimationEnd;
 
     public void ShootAnimationEndCallback(int _)
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [SerializeField] private Animator _weaponAnimator;
+    [SerializeField] private AnimationEventsHandler _animationEventsHandler;
+    [SerializeField] private ParticleSystem _shootVFX;
+
+
+    private bool _isCanShoot;
+
+    private void OnEnable()
+    {
+        _weaponAnimator.Play("Armature|Take");
+        _isCanShoot = true;
+        _animationEventsHandler.OnShootAnimationEnd += SetShootAvailavleState;
+        _animationEventsHandler.OnReloadAnimationEnd += SetShootAvailavleState;
+    }
+
+
+    private void OnDisable()
+    {
+        _animationEventsHandler.OnShootAnimationEnd -= SetShootAvailavleState;
+        _animationEventsHandler.OnReloadAnimationEnd -= SetShootAvailavleState;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _isCanShoot)
+        {
+            Debug.Log("Shoot");
+            _shootVFX.Play();
+            _weaponAnimator.Play("Armature|Shoot");
+        }
+        else if(Input.GetKeyDown(KeyCode.R) && _isCanShoot)
+        {
+            _weaponAnimator.Play("Armature|Reload");
+        }
+    }
+
+    private void SetShootAvailavleState()
+    {
+        _isCanShoot = true;
+    }
+}
