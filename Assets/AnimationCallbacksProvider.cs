30f2a7d81d5329691411453c7c82c8dfdc37fe61
CyberBread <ya.bolotov-ilya@yandex.ru>
2024-03-04 21:25:44 +0300

    add magnum

---
+++ Assets/AnimationCallbacksProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AnimationCallbacksProvider 
+{
+    public event Action OnShootAnimationEnd;
+    public event Action OnReloadAnimationStart;
+
+
+    static private AnimationCallbacksProvider _instance;
+    public static AnimationCallbacksProvider Instance => _instance;
+
+    static AnimationCallbacksProvider()
+    {
+        _instance = new AnimationCallbacksProvider();
+    }
+}
---
+++ Assets/AnimationEventsHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventsHandler : MonoBehaviour
+{
+    public event Action OnShootAnimationEnd;
+
+    public void ShootAnimationEndCallback(int _)
+    {
+        Debug.Log("End");
+        OnShootAnimationEnd?.Invoke();
+    }
+}
